2d593be82e48db7b801b3a0fe8313e939e31c6d8
UwaseShantal <uwaseshantal8@gmail.com>
2024-11-13 15:35:05 +0300

    Add files via upload

---
+++ task4.js
@@ -0,0 +1,10 @@
+//calculate area using arrow function
+let calculatearea= (length, width)=>{
+    return length * width
+    
+}
+
+let length = 10;
+let width = 5;
+let area = calculatearea(length, width);
+console.log("area is:", area);
